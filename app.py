fc51b978f3c263e885700a0c2976f26e345d1109
Alexis <axvacae@utn.edu.ec>
2024-07-09 19:50:53 -0500

    Update app to use environment variable PORT

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PIL import Image
 import torch
 from ultralytics import YOLO
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def detect_objects():
     return jsonify(detections)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
